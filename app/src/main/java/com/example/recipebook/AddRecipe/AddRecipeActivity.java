bebb607b5e2f0915a70fa33db2a3f18fb7e84a2a
Natan Z <natan.zaizev@gmail.com>
2021-07-18 23:02:22 +0300

    updates

--- app/src/main/java/com/example/recipebook/AddRecipe/AddRecipeActivity.java
+++ app/src/main/java/com/example/recipebook/AddRecipe/AddRecipeActivity.java
@@ -6,19 +6,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
 import com.example.recipebook.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class AddRecipeActivity extends AppCompatActivity {
 
-    RecyclerView instructionsRecycler,ingridients;
-    InstructionsAdapter instructionsAdapter;
+    RecyclerView instructionsRecycler,ingredientsRecycler;
+    InstructionsAdapter instructionsAdapter,ingredientsAdapter;
     ArrayList<String> instructions = new ArrayList<>();
-    EditText instruction;
+    ArrayList<String> ingredients = new ArrayList<>();
+    TextInputEditText instructionTextInput,ingredientTextInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +32,66 @@ public class AddRecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_recipe);
 
-        instruction = (EditText)findViewById(R.id.et_instruction);
-        instructionsRecycler = findViewById(R.id.InstructionsRecyclerView);
+        instructionTextInput = (TextInputEditText)findViewById(R.id.et_instruction);
+        ingredientTextInput = (TextInputEditText)findViewById(R.id.et_addIngredient);
 
+        instructionsRecycler = findViewById(R.id.InstructionsRecyclerView);
+        ingredientsRecycler = findViewById(R.id.IngredientsRecyclerView);
+        final TextInputLayout textInputLayout = findViewById(R.id.textInput_instruction);
+        textInputLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addInstruction();
+            }
+        });
+
+        final TextInputLayout ingredientInputLayout = findViewById(R.id.textInput_ingredient);
+        ingredientInputLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addIngredient();
+            }
+        });
 
         instructionsRecycler.setLayoutManager(new LinearLayoutManager(this));
-
         instructionsAdapter = new InstructionsAdapter(this,instructions);
-
         instructionsRecycler.setAdapter(instructionsAdapter);
 
+        ingredientsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        ingredientsAdapter= new InstructionsAdapter(this,ingredients);
+        ingredientsRecycler.setAdapter(ingredientsAdapter);
+
+        populateDropdown();
     }
 
-    public void addInstruction(View view) {
-        instructions.add(0,instruction.getText().toString());
-        Objects.requireNonNull(instructionsRecycler.getLayoutManager()).scrollToPosition(0);
-        instructionsAdapter.notifyItemInserted(0);
+    public void addInstruction() {
+        if(!instructionTextInput.getText().toString().isEmpty()) {
+            instructions.add(0, instructionTextInput.getText().toString());
+            Objects.requireNonNull(instructionsRecycler.getLayoutManager()).scrollToPosition(0);
+            instructionsAdapter.notifyItemInserted(0);
+        }
+    }
+
+    public void addIngredient(){
+        if(!ingredientTextInput.getText().toString().isEmpty()){
+            ingredients.add(0,ingredientTextInput.getText().toString());
+            Objects.requireNonNull(ingredientsRecycler.getLayoutManager()).scrollToPosition(0);
+            ingredientsAdapter.notifyItemInserted(0);
+        }
+    }
+
+    private void populateDropdown(){
+
+        String[] options = new String[] {"Breakfast", "Brunch", "Dinner", "Desert"};
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(
+                        this,
+                        R.layout.meal_dropdown_menu_item,
+                        options);
+
+        AutoCompleteTextView editTextFilledExposedDropdown =
+                findViewById(R.id.dropdown);
+        editTextFilledExposedDropdown.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/recipebook/AddRecipe/IngredientsAdapter.java
+++ app/src/main/java/com/example/recipebook/AddRecipe/IngredientsAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,24 +12,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.recipebook.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsAdapter.InstructionsViewHolder> {
-    Context context;
+public class IngredientsAdapter extends RecyclerView.Adapter<InstructionsAdapter.InstructionsViewHolder> {
+    ArrayList<String> ingredients;
 
-    ArrayList<String> instructions;
-
-    public InstructionsAdapter(Context ct,ArrayList<String> instructions){
-        this.context = ct;
-        this.instructions = instructions;
+    public IngredientsAdapter(Context context, ArrayList<String> ingredients){
+        this.ingredients = ingredients;
     }
-
     @NonNull
     @Override
-    public InstructionsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public InstructionsAdapter.InstructionsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.instructions_ingridients, parent , false);
-        return new InstructionsViewHolder(view,instructions.get(0));
+        return new InstructionsAdapter.InstructionsViewHolder(view,ingredients.get(0));
     }
 
     @Override
@@ -40,18 +34,16 @@ public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsAdapte
 
     @Override
     public int getItemCount() {
-        return instructions.size();
+        return 0;
     }
 
-    public static class InstructionsViewHolder extends RecyclerView.ViewHolder{
+
+    public static class IngredientsViewHolder extends RecyclerView.ViewHolder{
         EditText dataText;
-        public InstructionsViewHolder(@NonNull View itemView,String text) {
+        public IngredientsViewHolder(@NonNull View itemView,String text) {
             super(itemView);
             dataText = (EditText)itemView.findViewById(R.id.text);
             dataText.setText(text);
         }
-
     }
 }
-
-
